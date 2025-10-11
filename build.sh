dec788ccb78c481143bffa8f4c9cf726a99e91c3
Alun James <alunjames@gmail.com>
2025-10-11 21:04:51 +0100

    added cf purge

--- build.sh
+++ build.sh
@@ -10,4 +10,13 @@ source venv/bin/activate
 echo "Building MkDocs site..."
 mkdocs build
 
-echo "Build complete!"
+echo "Build complete! Purging cache..."
+
+# Get cloudflare creds
+source /home/alun/.cloudflare_creds
+
+# Purge cache
+curl -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" \
+  -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+  -H "Content-Type: application/json" \
+  --data '{"purge_everything":true}'
--- pull.sh
+++ pull.sh
@@ -6,8 +6,5 @@ git pull
 after=$(git rev-parse HEAD)
 
 if [ "$before" != "$after" ]; then
-    echo "$(date): Changes detected, building..." >> /home/alun/www/alunjam.es/build.log
     ./build.sh
-else
-    echo "$(date): No changes" >> /home/alun/www/alunjam.es/build.log
 fi
